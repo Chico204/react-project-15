093c5f76517e738db776c58f84b9295849896b9b
Chico204 <olatunjimark391@gmail.com>
2025-06-04 13:54:53 +0100

    second

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = () => {
  return (
-  <div className="loader">
-  </div>
+  <div className="loader w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+
  )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,21 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav className='navbar'>
-     navbar
-    </nav>
+    <nav class="bg-white h-20 flex items-center justify-between border-primary shadow-md">
+ <h1 className='text-2xl'>MixMaster</h1>
+ 
+   
+    <div class="flex items-center space-x-2">
+     
+      <Link to='/' class="capitalize font-normal tracking-widest text-lg px-2 py-1 transition-colors duration-300 hover:text-green-500">
+        home
+      </Link>
+      <Link to='/about' class="capitalize font-normal tracking-widest text-lg px-2 py-1 transition-colors duration-300 hover:text-green-500">
+        about
+      </Link>
+      
+    </div>
+ 
+</nav>
   )
 }
