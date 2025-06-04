d112477aa981be5a9fc868b102fe666b6c0e4f14
Chico204 <olatunjimark391@gmail.com>
2025-06-04 14:27:17 +0100

    fourth

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Cocktail({ image, name, id, info, glass }) {
   return (
-    <article className='cocktail'>
-   <div className='img-container'>
-    <img src={image} alt={name} />
-    
+    <article className='cocktail bg-white mb-8 shadow-sm hover:shadow-lg transition-shadow grid grid-rows-[auto_1fr] rounded-lg overflow-hidden ' >
+   <div >
+    <img className='img-container h-80 w-full object-cover rounded-t-lg' src={image} alt={name} />
+
+   </div>
+   <div className="cocktail-footer p-6">
+    <h3 className='text-2xl font-bold '>{name}</h3>
+    <h4 className='mb-1'>{glass} </h4>
+    <p className='text-gray-600 mb-2'>{info}</p>
+    <Link to={`/cocktail/${id}`} className='btn capitalize text-white bg-green-500 px-3 py-1 rounded hover:bg-green-700'> 
+    details</Link>
    </div>
     </article>
   )
--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -16,11 +16,11 @@ export default function CocktailList() {
     )
   }
   return(
-   <section className='section'>
-    <h2 className='section-title'>
+   <section className='section py-20'>
+    <h2 className='section-title text-2xl capitalize tracking-widest text-center mt-4 mb-14 '>
       cocktails
     </h2>
-    <div className='cocktails-center'>
+    <div className='cocktails-center grid gap-8 w-[85vw] max-w-[1170px] mx-auto sm:grid-cols-[repeat(auto-fill,_minmax(338.8px,_1fr))]'>
    {cocktails.map((item)=> {
     return<Cocktail key={item.id} {...item}/>
    })}
